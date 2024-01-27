964946a21528c8f7f93d9595b140cddee2d7e68a
Jade1724 <haruka.i1724@gmail.com>
2024-01-28 12:35:29 +1300

    Timer text

--- Assets/GGJ2024/Scripts/GameManager.cs
+++ Assets/GGJ2024/Scripts/GameManager.cs
@@ -4,10 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    enum CookingPhase {
+        Standby,
+        Gathering,
+        Cutting,
+        Cooking,
+        Serving
+    };
+
+    public const int COOKING_DURATION = 60; 
     public int score;
 
+    private CookingPhase currentPhase = CookingPhase.Standby;
+    public int remainingTime = COOKING_DURATION;
+    private int remainingLife;
+    private bool isCountingDown;
+
+
      // Singleton instance
-    private static GameManager instance;
+    public static GameManager instance;
 
     private void Awake() {
          // Ensure only one instance exists
@@ -25,13 +40,32 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("Game manager started");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (currentPhase != CookingPhase.Standby && !isCountingDown) {
+            isCountingDown = true;
+            InvokeRepeating("CountDownTime", 0f, 1f);
+        } else {
+            if (isCountingDown) {
+                CancelInvoke("CountDownTime");
+            }
+        }
     }
 
+    void CountDownTime() {
+        remainingTime--;
+        if (remainingTime <= 0) {
+                remainingLife--;
+                if (remainingLife == 0) {
+                    Debug.Log("Game Over");
+                } else {
+                    remainingTime = COOKING_DURATION;
+                    currentPhase = CookingPhase.Standby;
+                }
+            }
+    }
 }
